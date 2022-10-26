618502c0b303ab89948adabb63f89ff596d18979
davide99 <info.davide99@gmail.com>
2022-10-26 13:48:48 +0200

    Activity licenze + tema nero

--- app/src/main/java/com/davide99/alextuner/AboutActivity.java
+++ app/src/main/java/com/davide99/alextuner/AboutActivity.java
@@ -2,13 +2,25 @@ package com.davide99.alextuner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
+
+import com.davide99.alextuner.databinding.ActivityAboutBinding;
+
 
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+
+        ActivityAboutBinding binding = ActivityAboutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.licenzeBtn.setOnClickListener((View v) -> {
+            startActivity(new Intent(this, OssLicensesMenuActivity.class));
+        });
     }
 }
